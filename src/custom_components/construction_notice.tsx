462286e21b0037bc96f82fcda246b4f1aa6daef0
ZachMc21-OW <zach.magloughlin@omniaworkspace.com>
2025-10-22 14:39:06 -0600

    Fixed some styling on the homepage. Added Router to construction_notice.tsx

--- src/custom_components/construction_notice.tsx
+++ src/custom_components/construction_notice.tsx
@@ -1,4 +1,4 @@
-
+import { useNavigate } from 'react-router-dom';
 import ConstructionIcon from '@mui/icons-material/Construction';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
@@ -7,15 +7,17 @@ interface ConstructionProps {
 }
 
 const ConstructionNotice = ({ title }: ConstructionProps) => {
+    const navigate = useNavigate();
+
     return(
         <div className="construction-notice">
             <ConstructionIcon />
             <h1>{title}</h1>
             <p>This page is under construction. Check back later!</p>
-            <a href=""> { /* TODO */}
+            <button onClick={() => navigate(-1)}>
                 Go back
                 <ArrowBackIcon />
-            </a>
+            </button>
         </div>
     );
 }
