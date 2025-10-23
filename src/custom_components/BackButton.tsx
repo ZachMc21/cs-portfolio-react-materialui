8edf098d1f53be9b9bdd2acbf9ba57b29d887a15
ZachMc21-OW <zach.magloughlin@omniaworkspace.com>
2025-10-23 04:28:12 -0600

    Destroyed commit has been redone

--- src/custom_components/BackButton.tsx
+++ src/custom_components/BackButton.tsx
@@ -2,7 +2,7 @@ import { styled } from "@mui/material/styles";
 import MuiButton from "@mui/material/Button";
 
 export const BackButton = styled(MuiButton)(({ }) => ({
-    padding: '1rem',
+    padding: '2rem',
     flexDirection: "column",
     justifyContent: "center",
     margin: "1rem",
