93f6665f7529acb677e9eaa4dd85b64ce7f62d95
ZachMc21-OW <zach.magloughlin@omniaworkspace.com>
2025-10-25 11:10:24 -0600

    Revert "Destroyed commit has been redone"
    This reverts commit 8edf098d1f53be9b9bdd2acbf9ba57b29d887a15.

--- src/custom_components/BackButton.tsx
+++ src/custom_components/BackButton.tsx
@@ -2,7 +2,7 @@ import { styled } from "@mui/material/styles";
 import MuiButton from "@mui/material/Button";
 
 export const BackButton = styled(MuiButton)(({ }) => ({
-    padding: '2rem',
+    padding: '1rem',
     flexDirection: "column",
     justifyContent: "center",
     margin: "1rem",
