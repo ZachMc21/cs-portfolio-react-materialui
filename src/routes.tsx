8f69dd3cbf87e5fff06c0aa0e9cd38ee4c78b4c6
ZachMc21-OW <zach.magloughlin@omniaworkspace.com>
2025-10-25 11:16:52 -0600

    Cleanup after the revert

---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import App from "./App";
+import CSPortfolio from "./pages/CSPortfolio";
+
+export const routes = [
+    {
+        path: "/",
+        component: App
+    },
+    {
+        path: "/cs-portfolio",
+        component: CSPortfolio
+    }
+];
