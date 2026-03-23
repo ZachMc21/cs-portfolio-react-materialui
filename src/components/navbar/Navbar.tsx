82abd9ad86ea51a4beaba3f1fd1f0ec42c2acad4
ZachMc21 <magloughlin.zach@gmail.com>
2026-03-23 15:05:51 -0600

    Tidy up navbar. Next up: add to other pages and make active link change.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,8 +3,8 @@ import homeTheme from "../../styles/homeTheme"
 import { Link } from "react-router";
 import "./navbar.css";
 
-const navbarTextList: string[] = ["CS Portfolio", "Photography Portfolio", "Music Database", "Text RPG"];
-const navbarLinksList: string[] = ["cs-portfolio", "photography-portfolio", "music-database", "text-rpg"];
+//const navbarTextList: string[] = ["CS Portfolio", "Photography Portfolio", "Music Database", "Text RPG"];
+//const navbarLinksList: string[] = ["cs-portfolio", "photography-portfolio", "music-database", "text-rpg"];
 
 interface NavbarProps {
 	currentPage: string;
@@ -23,10 +23,10 @@ const Navbar = ({ currentPage }: NavbarProps) => {
 			<CssBaseline />
 
             <Box id="pages-container" sx = {{ display: "flex", flexDirection: "row", justifyContent: "flex-start", alignItems: "center", gap: "2em" }}>
-                {/* <Link to="#" className="navbar-link navbar-link-active">CS Portfolio</Link>
+                <Link to="#" className="navbar-link navbar-link-active">CS Portfolio</Link>
                 <Link to="/photography-portfolio" className="navbar-link">Photography Portfolio</Link>
                 <Link to="/music-database" className="navbar-link">Music Database</Link>
-                <Link to="/text-rpg" className="navbar-link">Text RPG</Link> */}
+                <Link to="/text-rpg" className="navbar-link">Text RPG</Link>
             </Box>
 
             <Link to="/" replace className="navbar-link navbar-link-home">Return to <br /> Landing Page</Link>
@@ -36,28 +36,27 @@ const Navbar = ({ currentPage }: NavbarProps) => {
     )
 };
 
-const pagesContainer = document.getElementById("pages-container");
+// const pagesContainer = document.getElementById("pages-container");
 
-function buildNavbarChild(text: string, link: string): HTMLElement {
-    const item = document.createElement("a");
-    item.textContent = text;
-    item.href = "/" + link;
-    item.className = "navbar-link";
-    // if(currentPage) {
-    //     item.href = "#";
-    //     item.classList.add("navbar-link-active");
-    // }
-    return item;
-}
+// function buildNavbarChild(text: string, link: string): HTMLElement {
+//     const item = document.createElement("a");
+//     item.textContent = text;
+//     item.href = "/" + link;
+//     item.className = "navbar-link";
+//     // if(currentPage) {
+//     //     item.href = "#";
+//     //     item.classList.add("navbar-link-active");
+//     // }
+//     return item;
+// }
 
-if(pagesContainer == null) {
-    console.log("Could not load navbar");
-}
-else {
-    for(let i=0; i<4; i++) {
-        pagesContainer!.appendChild(buildNavbarChild(navbarTextList[i], navbarLinksList[i]));
-    }
-
-}
+// if(pagesContainer == null) {
+//     console.log("Could not load navbar");
+// }
+// else {
+//     for(let i=0; i<4; i++) {
+//         pagesContainer!.appendChild(buildNavbarChild(navbarTextList[i], navbarLinksList[i]));
+//     }
+// }
 
 export default Navbar;
--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -3,35 +3,27 @@
 
 const typography = {
     fontFamily: ['aller', 'sans-serif'].join(','),
-    allVariants: {
-        fontWeight: "regular",
-    },
     title: {
         fontFamily: ['eightgon', 'sans-serif'].join(','),
-        fontWeight: "regular",
         textAlign: "center",
         fontSize: "3rem",
     },
     subtitle: {
         fontFamily: ['eightgon', 'sans-serif'].join(','),
-        fontWeight: "regular",
         fontStyle: "italic",
         textAlign: "center",
     },
     h1: {
         fontFamily: ['eightgon', 'sans-serif'].join(','),
-        fontWeight: "regular",
         fontSize: "2rem",
     },
     bodyRight: {
         fontFamily: ['aller', 'sans-serif'].join(','),
-        fontWeight: "regular",
         fontSize: "1.15rem",
         textAlign: "right",
     },
     bodyCenter: {
         fontFamily: ['aller', 'sans-serif'].join(','),
-        fontWeight: "regular",
         fontSize: "1.15rem",
         textAlign: "center",
     }
