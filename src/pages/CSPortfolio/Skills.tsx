a7135132cdec83476a38fe03b26723170dcddda1
ZachMc21 <magloughlin.zach@gmail.com>
2026-04-05 14:15:15 -0600

    Animated CSPortfolio/Skills with basic flip animation.

--- src/pages/CSPortfolio/Skills.tsx
+++ src/pages/CSPortfolio/Skills.tsx
@@ -3,18 +3,15 @@ import { Box, Container, ThemeProvider, Typography } from '@mui/material';
 import Footer from '../../components/Footer';
 
 import DocumentTitle from '../../utils/DocumentTitle';
-import ScrollAnimate from "../../utils/ScrollAnimate";
 import NavbarCS from "../../components/navbar/NavbarCS";
 
 import csLight from '../../styles/csLight';
 import "./Skills.css";
 import "../../styles/animations.css";
-import "../../styles/scss/agency.scss";
 
 function Skills() {
 
     DocumentTitle("Skills | CS Portfolio");
-    ScrollAnimate();
 	
 	return (
 		<Container sx={{ mt: 12 }}>
@@ -34,7 +31,7 @@ function Skills() {
                             <Typography className="h2" sx={{ color: csLight.palette.secondary.contrastText }}>
                                 Core
                             </Typography>
-                            <Box className="tech-stack-container anim-delay-0">
+                            <Box className="tech-stack-container">
                                 <img className="tech-logo" src="https://upload.wikimedia.org/wikipedia/commons/8/80/HTML5_logo_resized.svg" height="40" width="auto" />
                                 <img className="tech-logo" src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" height="40" width="auto" />
                                 <img className="tech-logo" src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Javascript-shield.svg" height="40" width="auto" />
@@ -62,8 +59,8 @@ function Skills() {
                             <Typography className="h2" sx={{ color: csLight.palette.secondary.contrastText }}>
                                 Design Languages
                             </Typography>
-                            <Box id="mui-logo">
-                                <img src="/src/assets/logos/mui.png" />
+                            <Box id="mui-logo" className="tech-stack-container">
+                                <img className="tech-logo" src="/src/assets/logos/mui.png" />
                                 {/* SVG attempt
                                 <svg viewBox="0 0 100 100" preserveAspectRatio="xMaxYMid meet" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="#0073E6" fillRule="evenodd" d="M24 5.601V1.592a.344.344 0 0 0-.514-.298l-2.64 1.508a.688.688 0 0 0-.346.597v4.009c0 .264.285.43.514.298l2.64-1.508A.688.688 0 0 0 24 5.6ZM.515 1.295l7.643 4.383a.688.688 0 0 0 .684 0l7.643-4.383a.344.344 0 0 1 .515.298v12.03c0 .235-.12.453-.319.58l-4.65 2.953 3.11 1.832c.22.13.495.127.713-.009l4.61-2.878a.344.344 0 0 0 .161-.292v-4.085c0-.254.14-.486.362-.606l2.507-1.346a.344.344 0 0 1 .506.303v7.531c0 .244-.13.47-.34.593l-7.834 4.592a.688.688 0 0 1-.71-.009l-5.953-3.681A.344.344 0 0 1 9 18.808v-3.624c0-.115.057-.222.153-.286l4.04-2.694a.688.688 0 0 0 .307-.572v-4.39a.137.137 0 0 0-.208-.117l-4.44 2.664a.688.688 0 0 1-.705.002L3.645 7.123a.138.138 0 0 0-.208.118v7.933a.344.344 0 0 1-.52.295L.5 14.019C.19 13.833 0 13.497 0 13.135V1.593c0-.264.286-.43.515-.298Z" clipRule="evenodd"/></svg>
                                  */}
